4460bfab12a8ccfb3eb4a3ae6b5e94c883a36b74
Anubhooti Jain <Anubhooti Jain@LAPTOP-JOMJTKUO>
2020-01-28 22:28:07 +0530

    Added a new Java File

---
+++ Facebook/src/Fb/GitTest.java
@@ -0,0 +1,19 @@
+package Fb;
+
+public class GitTest {
+	
+	public static void sum(int i, int j) {
+		
+		int sum = i+j;
+		
+		System.out.println(sum);
+	}
+
+	public static void main(String[] args) {
+		sum(10,20);
+		
+		
+
+	}
+
+}
