c4767f43a62ab5a26011b4f58abe6c25efa29e40
Anubhooti Jain <Anubhooti Jain@LAPTOP-JOMJTKUO>
2020-01-28 22:33:52 +0530

    Add another Method

---
+++ Facebook/src/Fb/GitTest2.java
@@ -0,0 +1,16 @@
+package Fb;
+
+public class GitTest2 {
+	
+	public static int multiply(int i,int j) {
+		
+		return i*j;
+		
+	}
+
+	public static void main(String[] args) {
+	
+		System.out.println(multiply(10,20));
+	}
+
+}
